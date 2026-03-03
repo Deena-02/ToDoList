ba9cb792e2a8b5371dbeea87487d9d72ea333ba7
Deena <dearsifengsgirl45@gmail.com>
2026-03-03 14:07:34 +0530

    modified mongodb uri commit

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/TodoList',
-    console.log('MongoDB connected')
-)
+const uri = process.env.MONGO_URI;
 
-app.listen(5000,
-    console.log('Server listening on port: 5000')
-)
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
 
 app.post('/add', (req, res) => {
   const { task } = req.body;
